6ce5f11043e49393dd43d498fcacd7649486b614
Marc Boisvert-Dupras <marc@tamarak.dev>
2024-12-15 12:14:05 -0500

    feat(rose-pine): customize color scheme for improved UI contrast
    Enhance Neovim's Rose Pine theme with custom palette and highlight groups
    for better visibility and aesthetics in various UI elements, including
    status line, Telescope, and search highlights.

--- .config/nvim/lua/plugins/rose-pine.lua
+++ .config/nvim/lua/plugins/rose-pine.lua
@@ -2,10 +2,31 @@ return {
   "rose-pine/neovim",
   name = "rose-pine",
   opts = {
+    palette = {
+      main = {
+        base = "#000000",
+      },
+    },
     highlight_groups = {
       FlashLabel = { fg = "base", bg = "leaf", inherit = false },
       CurSearch = { fg = "text", bg = "leaf", blend = 20, inherit = false },
       Search = { fg = "text", bg = "leaf", blend = 20, inherit = false },
+
+      StatusLine = { fg = "love", bg = "love", blend = 10 },
+      StatusLineNC = { fg = "subtle", bg = "surface" },
+
+      TelescopeBorder = { fg = "overlay", bg = "overlay" },
+      TelescopeNormal = { fg = "subtle", bg = "overlay" },
+      TelescopeSelection = { fg = "text", bg = "highlight_med" },
+      TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
+      TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
+
+      TelescopeTitle = { fg = "base", bg = "love" },
+      TelescopePromptTitle = { fg = "base", bg = "pine" },
+      TelescopePreviewTitle = { fg = "base", bg = "iris" },
+
+      TelescopePromptNormal = { fg = "text", bg = "surface" },
+      TelescopePromptBorder = { fg = "surface", bg = "surface" },
     },
   },
 }
