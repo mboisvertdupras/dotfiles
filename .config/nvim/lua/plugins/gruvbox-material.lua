7c1d75acc1cf6ebcd1a4a642a42336f7a8e07ae0
Marc Boisvert-Dupras <marc@tamarak.dev>
2024-12-27 13:43:54 -0500

    style: Configure gruvbox-material with pure black background

--- .config/nvim/lua/plugins/gruvbox-material.lua
+++ .config/nvim/lua/plugins/gruvbox-material.lua
@@ -1,3 +1,11 @@
 return {
   "sainnhe/gruvbox-material",
+  config = function()
+    vim.g.gruvbox_material_background = "hard"
+    vim.g.gruvbox_material_better_performance = 1
+    vim.g.gruvbox_material_transparent_background = 0
+    vim.g.gruvbox_material_override = {
+      Normal = { bg = "#000000" },
+    }
+  end,
 }
