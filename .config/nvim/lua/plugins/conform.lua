3d7cf98362668252a2b7bd0b03fd1d28bac6a0a5
Marc Boisvert-Dupras <m.boisvert-dupras@dialekta.com>
2025-03-05 11:36:12 -0500

    refactor: Update formatters and add phpcbf configuration in conform.lua

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -5,12 +5,20 @@ return {
     opts = {
       quiet = true,
       formatters_by_ft = {
-        php = { "pint" },
+        -- html = { "prettier" },
+        php = { "phpcbf" },
         vue = { "volar", "eslint" },
         javascript = { "eslint" },
         javascriptreact = { "eslint" },
         blade = { "blade-formatter" },
       },
+      formatters = {
+        phpcbf = {
+          command = "phpcbf",
+          args = { "--standard=PSR12", "$FILENAME" },
+        },
+      },
+      notify_on_error = true,
     },
   },
 }
