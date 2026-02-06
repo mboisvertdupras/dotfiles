f976536f316f7b045c5cd479807782e42015cf3d
Marc Boisvert-Dupras <m.boisvert-dupras@dialekta.com>
2026-02-06 10:16:55 -0500

    php formatting

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,36 +2,32 @@ return {
   {
     "stevearc/conform.nvim",
     event = "LspAttach",
-    opts = {
-      quiet = true,
-      formatters_by_ft = {
-        -- html = { "prettier" },
-        php = function(bufnr)
-          -- Check for HTML tags in first 50 lines
-          for i = 0, math.min(49, vim.api.nvim_buf_line_count(bufnr) - 1) do
-            local line = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)[1]
-            if line:match("<%a+") then
-              return { "phpcbf" }
-            end
-          end
-          return { "pint" }
-        end,
-        vue = { "volar", "eslint" },
-        javascript = { "eslint" },
-        javascriptreact = { "eslint" },
-        blade = { "blade-formatter" },
-      },
-      formatters = {
-        phpcbf = {
-          command = "phpcbf",
-          args = { "--standard=PSR12", "$FILENAME" },
+    opts = function()
+      local util = require("conform.util")
+
+      return {
+        quiet = true,
+        formatters_by_ft = {
+          -- html = { "prettier" },
+          php = { "pint" },
+          vue = { "volar", "eslint" },
+          javascript = { "eslint" },
+          javascriptreact = { "eslint" },
+          blade = { "blade-formatter" },
         },
-        pint = {
-          command = "pint",
-          args = { "$FILENAME" },
+        formatters = {
+          pint = {
+            command = util.find_executable({ "vendor/bin/pint", "pint" }, "pint"),
+            args = { "$FILENAME" },
+          },
+          ["blade-formatter"] = {
+            command = util.find_executable({ "node_modules/.bin/blade-formatter", "blade-formatter" }, "blade-formatter"),
+            args = { "--write", "--sort-tailwindcss-classes", "$FILENAME" },
+            stdin = false,
+          },
         },
-      },
-      notify_on_error = true,
-    },
+        notify_on_error = true,
+      }
+    end,
   },
 }
